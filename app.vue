5259d539d1ac6618d8f155416d4086b774598048
unknown <yehiabassam94@gmail.com>
2022-11-10 20:49:45 +0200

    restructure project

--- app.vue
+++ app.vue
@@ -1,114 +1,71 @@
 <script setup lang="ts">
-import { names } from './names';
-
-const items = ref([
+import { Gender, Popularity, Length, names } from "@/names";
+interface OptionsState {
+  gender: Gender;
+  popularity: Popularity;
+  length: Length;
+}
+const options = reactive<OptionsState>({
+  gender: Gender.GIRL,
+  length: Length.SHORT,
+  popularity: Popularity.TRENDY,
+});
+const computeSelectedNames = () => {
+  const filteredNames = names
+    .filter((name) => name.gender === options.gender)
+    .filter((name) => name.popularity === options.popularity)
+    .filter((name) => {
+      if (options.length === Length.ALL) return true;
+      else return name.length === options.length;
+    });
+  selectedNames.value = filteredNames.map((name) => name.name);
+};
+const selectedNames = ref<string[]>([]);
+const removeName = (index: number) => {
+  const filteredNames = [...selectedNames.value];
+  filteredNames.splice(index, 1);
+  selectedNames.value = filteredNames;
+};
+const optionsArray = [
   {
     title: "1) Choose a gender",
-    options: [
-      { label: "Boy", active: true },
-      { label: "Girl", active: false },
-    ],
+    category: "gender",
+    buttons: [Gender.GIRL, Gender.BOY],
   },
   {
     title: "2) Choose the name's popularity",
-    options: [
-      { label: "Trendy", active: false },
-      { label: "Unique", active: true },
-    ],
+    category: "popularity",
+    buttons: [Popularity.TRENDY, Popularity.UNIQUE],
   },
   {
     title: "3) Choose name's length",
-    options: [
-      { label: "All", active: true },
-      { label: "Long", active: false },
-      { label: "Short", active: false },
-    ],
+    category: "length",
+    buttons: [Length.SHORT, Length.ALL, Length.LONG],
   },
-]);
-
-function borderStyle (index: number, options: Array) {
-  let classStytle;
-  if (index == 2) 
-    classStytle = "option-right";
-  else if (index == 1 && options.length == 3) 
-    classStytle = "option";
-  else if (index == 1 && options.length != 3) 
-    classStytle = "option-right";
-  else 
-    classStytle = "option-left";
-
-  return classStytle;
-}
-
-// set active class
-const setActiveClass = (clickedOption: Object, options: Object[]) => {
-  for (const option of options) {
-    option.active = false;
-  }
-  clickedOption.active = true;
-
-  selectOptions();
-};
-
-// selected options
-let selectedOptions = reactive <object[]> ([]);
-function selectOptions(){
-  selectedOptions = [];
-  items.value.forEach((item) => {
-    item.options.forEach((type) => {
-      type.active ? selectedOptions.push(type.label) : null;
-    })
-  })
-};
-
-// selected names
-const selectedNames = ref <object[]> ([]);
-const selectNames = () => {
-  const filteredNames = names
-  .filter( item => item.gender == selectedOptions[0])
-  .filter( item => item.popularity == selectedOptions[1])
-  .filter( item => {
-      if ( selectedOptions[2] === "All") return true;
-      else return item.length === selectedOptions[2];
-    });
-    
-  selectedNames.value = filteredNames.map((item) => item.name);
-};
-
-onMounted(() => {
-  selectOptions();
-})
-
+];
 </script>
 
 <template>
   <div class="container">
     <h1>Baby Name Generator</h1>
     <p>Choose your options and click the "Find Names" buttom below</p>
-
     <div class="options-container">
-      <div v-for="(item, i) in items" :key="i" class="option-container">
-        <h4>{{ item.title }}</h4>
-        <div class="option-buttons">
-          <button
-            v-for="(option, j) in item.options"
-            :key="j"
-            :class="
-              borderStyle(j, item.options),
-              option.active ? 'option-active' : null
-            "
-            class="option"
-            @click="setActiveClass(option, item.options)"
-          >
-            {{ option.label }}
-          </button>
-        </div>
-      </div>
-      <button class="primary" @click="selectNames">Find names</button>
-
-      <div>
-        {{ selectedNames }}
-      </div>
+      <OptionsOption
+        v-for="option in optionsArray"
+        :key="option.title"
+        :option="option"
+        :options="options"
+      />
+      <button class="primary" @click="computeSelectedNames">Find Names</button>
+    </div>
+    <div class="cards-container">
+      <CardName
+        v-for="(name, index) in selectedNames"
+        :key="name"
+        :name="name"
+        @remove="() => removeName(index)"
+        :index="index"
+      />
     </div>
   </div>
 </template>
@@ -133,31 +90,6 @@ h1 {
   margin-top: 4rem;
   position: relative;
 }
-.option-container {
-  margin-bottom: 2rem;
-}
-.option {
-  background: white;
-  outline: 0.15rem solid rgb(249, 87, 89);
-  border: none;
-  padding: 0.75rem;
-  width: 12rem;
-  font-size: 1rem;
-  color: rgb(27, 60, 138);
-  cursor: pointer;
-  font-weight: 200;
-}
-.option-left {
-  border-radius: 1rem 0 0 1rem;
-}
-.option-right {
-  border-radius: 0 1rem 1rem 0;
-}
-
-.option-active {
-  background-color: rgb(249, 87, 89);
-  color: white;
-}
 .primary {
   background-color: rgb(249, 87, 89);
   color: white;
@@ -167,5 +99,10 @@ h1 {
   font-size: 1rem;
   margin-top: 1rem;
   cursor: pointer;
+}
+.cards-container {
+  display: flex;
+  margin-top: 3rem;
+  flex-wrap: wrap;
 }
 </style>
--- names.ts
+++ names.ts
@@ -1,268 +1,267 @@
 interface Name {
-  id: Number;
-  name: String;
-  gender: Gender;
-  popularity: Popularity;
-  length: Length;
-}
-
-enum Gender {
-  BOY = "Boy",
-  GIRL = "Girl",
-}
-
-enum Popularity {
-  TRENDY = "Trendy",
-  UNIQUE = "Unique",
-}
-
-enum Length {
-  ALL = "All",
-  LONG = "Long",
-  SHORT = "Short",
-}
-
-export const names: Name[] = [
-  {
-    id: 1,
-    name: "Mohamed",
-    gender: Gender.BOY,
-    popularity: Popularity.UNIQUE,
-    length: Length.LONG,
-  },
-  {
-    id: 2,
-    name: "Ali",
-    gender: Gender.BOY,
-    popularity: Popularity.TRENDY,
-    length: Length.SHORT,
-  },
-  {
-    id: 3,
-    name: "Lamelo",
-    gender: Gender.BOY,
-    popularity: Popularity.UNIQUE,
-    length: Length.SHORT,
-  },
-  {
-    id: 4,
-    name: "Abraham",
-    gender: Gender.BOY,
-    popularity: Popularity.TRENDY,
-    length: Length.LONG,
-  },
-  {
-    id: 5,
-    name: "Bartholomew",
-    gender: Gender.BOY,
-    popularity: Popularity.UNIQUE,
-    length: Length.LONG,
-  },
-  {
-    id: 6,
-    name: "Noah",
-    gender: Gender.BOY,
-    popularity: Popularity.TRENDY,
-    length: Length.SHORT,
-  },
-  {
-    id: 7,
-    name: "Adel",
-    gender: Gender.BOY,
-    popularity: Popularity.TRENDY,
-    length: Length.LONG,
-  },
-  {
-    id: 8,
-    name: "William",
-    gender: Gender.BOY,
-    popularity: Popularity.TRENDY,
-    length: Length.LONG,
-  },
-  {
-    id: 9,
-    name: "Lucus",
-    gender: Gender.BOY,
-    popularity: Popularity.TRENDY,
-    length: Length.SHORT,
-  },
-  {
-    id: 10,
-    name: "Harrison",
-    gender: Gender.BOY,
-    popularity: Popularity.UNIQUE,
-    length: Length.LONG,
-  },
-  {
-    id: 11,
-    name: "Selma",
-    gender: Gender.GIRL,
-    popularity: Popularity.UNIQUE,
-    length: Length.SHORT,
-  },
-  {
-    id: 12,
-    name: "Asher",
-    gender: Gender.BOY,
-    popularity: Popularity.UNIQUE,
-    length: Length.SHORT,
-  },
-  {
-    id: 13,
-    name: "Tucker",
-    gender: Gender.BOY,
-    popularity: Popularity.TRENDY,
-    length: Length.SHORT,
-  },
-
-  {
-    id: 14,
-    name: "Arya",
-    gender: Gender.GIRL,
-    popularity: Popularity.UNIQUE,
-    length: Length.SHORT,
-  },
-  {
-    id: 15,
-    name: "Olivia",
-    gender: Gender.GIRL,
-    popularity: Popularity.TRENDY,
-    length: Length.SHORT,
-  },
-  {
-    id: 16,
-    name: "Fay",
-    gender: Gender.GIRL,
-    popularity: Popularity.UNIQUE,
-    length: Length.SHORT,
-  },
-  {
-    id: 17,
-    name: "Brooklyn",
-    gender: Gender.GIRL,
-    popularity: Popularity.TRENDY,
-    length: Length.LONG,
-  },
-  {
-    id: 18,
-    name: "Genevieve",
-    gender: Gender.GIRL,
-    popularity: Popularity.UNIQUE,
-    length: Length.LONG,
-  },
-  {
-    id: 19,
-    name: "Zoe",
-    gender: Gender.GIRL,
-    popularity: Popularity.TRENDY,
-    length: Length.SHORT,
-  },
-  {
-    id: 20,
-    name: "Valentina",
-    gender: Gender.GIRL,
-    popularity: Popularity.TRENDY,
-    length: Length.LONG,
-  },
-  {
-    id: 21,
-    name: "Josephine",
-    gender: Gender.GIRL,
-    popularity: Popularity.TRENDY,
-    length: Length.LONG,
-  },
-  {
-    id: 22,
-    name: "Maya",
-    gender: Gender.GIRL,
-    popularity: Popularity.TRENDY,
-    length: Length.SHORT,
-  },
-  {
-    id: 23,
-    name: "Everleigh",
-    gender: Gender.GIRL,
-    popularity: Popularity.UNIQUE,
-    length: Length.LONG,
-  },
-  {
-    id: 24,
-    name: "Poppy",
-    gender: Gender.GIRL,
-    popularity: Popularity.UNIQUE,
-    length: Length.SHORT,
-  },
-  {
-    id: 25,
-    name: "Maia",
-    gender: Gender.GIRL,
-    popularity: Popularity.UNIQUE,
-    length: Length.SHORT,
-  },
-  {
-    id: 26,
-    name: "Ivy",
-    gender: Gender.GIRL,
-    popularity: Popularity.TRENDY,
-    length: Length.SHORT,
-  },
-
-  {
-    id: 27,
-    name: "Jude",
-    gender: Gender.GIRL,
-    popularity: Popularity.UNIQUE,
-    length: Length.SHORT,
-  },
-  {
-    id: 28,
-    name: "Adrian",
-    gender: Gender.BOY,
-    popularity: Popularity.TRENDY,
-    length: Length.SHORT,
-  },
-  {
-    id: 29,
-    name: "Sunny",
-    gender: Gender.GIRL,
-    popularity: Popularity.UNIQUE,
-    length: Length.SHORT,
-  },
-  {
-    id: 30,
-    name: "Channing",
-    gender: Gender.BOY,
-    popularity: Popularity.TRENDY,
-    length: Length.LONG,
-  },
-  {
-    id: 31,
-    name: "Tennessee",
-    gender: Gender.BOY,
-    popularity: Popularity.UNIQUE,
-    length: Length.LONG,
-  },
-  {
-    id: 32,
-    name: "Dallas",
-    gender: Gender.BOY,
-    popularity: Popularity.TRENDY,
-    length: Length.SHORT,
-  },
-
-  {
-    id: 33,
-    name: "Zephyr",
-    gender: Gender.BOY,
-    popularity: Popularity.UNIQUE,
-    length: Length.LONG,
-  },
-
-  {
-    id: 34,
-    name: "Teri",
-    gender: Gender.BOY,
-    popularity: Popularity.TRENDY,
-    length: Length.SHORT,
-  },
-];
+    id: number;
+    name: string;
+    gender: Gender;
+    popularity: Popularity;
+    length: Length;
+  }
+  
+  export enum Gender {
+    GIRL = "Girl",
+    BOY = "Boy",
+  }
+  
+  export enum Popularity {
+    TRENDY = "Trendy",
+    UNIQUE = "Unique",
+  }
+  
+  export enum Length {
+    SHORT = "Short",
+    LONG = "Long",
+    ALL = "All",
+  }
+  export const names: Name[] = [
+    {
+      id: 1,
+      name: "Laith",
+      gender: Gender.BOY,
+      popularity: Popularity.UNIQUE,
+      length: Length.SHORT,
+    },
+    {
+      id: 2,
+      name: "Jake",
+      gender: Gender.BOY,
+      popularity: Popularity.TRENDY,
+      length: Length.SHORT,
+    },
+    {
+      id: 3,
+      name: "Lamelo",
+      gender: Gender.BOY,
+      popularity: Popularity.UNIQUE,
+      length: Length.SHORT,
+    },
+    {
+      id: 4,
+      name: "Ali",
+      gender: Gender.BOY,
+      popularity: Popularity.TRENDY,
+      length: Length.LONG,
+    },
+    {
+      id: 5,
+      name: "Bartholomew",
+      gender: Gender.BOY,
+      popularity: Popularity.UNIQUE,
+      length: Length.LONG,
+    },
+    {
+      id: 6,
+      name: "Noah",
+      gender: Gender.BOY,
+      popularity: Popularity.TRENDY,
+      length: Length.SHORT,
+    },
+    {
+      id: 7,
+      name: "Mohamed",
+      gender: Gender.BOY,
+      popularity: Popularity.TRENDY,
+      length: Length.LONG,
+    },
+    {
+      id: 8,
+      name: "William",
+      gender: Gender.BOY,
+      popularity: Popularity.TRENDY,
+      length: Length.LONG,
+    },
+    {
+      id: 9,
+      name: "Lucus",
+      gender: Gender.BOY,
+      popularity: Popularity.TRENDY,
+      length: Length.SHORT,
+    },
+    {
+      id: 10,
+      name: "Harrison",
+      gender: Gender.BOY,
+      popularity: Popularity.UNIQUE,
+      length: Length.LONG,
+    },
+    {
+      id: 11,
+      name: "Selma",
+      gender: Gender.BOY,
+      popularity: Popularity.UNIQUE,
+      length: Length.SHORT,
+    },
+    {
+      id: 12,
+      name: "Asher",
+      gender: Gender.BOY,
+      popularity: Popularity.UNIQUE,
+      length: Length.SHORT,
+    },
+    {
+      id: 13,
+      name: "Tucker",
+      gender: Gender.BOY,
+      popularity: Popularity.TRENDY,
+      length: Length.SHORT,
+    },
+  
+    {
+      id: 14,
+      name: "Arya",
+      gender: Gender.GIRL,
+      popularity: Popularity.UNIQUE,
+      length: Length.SHORT,
+    },
+    {
+      id: 15,
+      name: "Olivia",
+      gender: Gender.GIRL,
+      popularity: Popularity.TRENDY,
+      length: Length.SHORT,
+    },
+    {
+      id: 16,
+      name: "Fay",
+      gender: Gender.GIRL,
+      popularity: Popularity.UNIQUE,
+      length: Length.SHORT,
+    },
+    {
+      id: 17,
+      name: "Brooklyn",
+      gender: Gender.GIRL,
+      popularity: Popularity.TRENDY,
+      length: Length.LONG,
+    },
+    {
+      id: 18,
+      name: "Genevieve",
+      gender: Gender.GIRL,
+      popularity: Popularity.UNIQUE,
+      length: Length.LONG,
+    },
+    {
+      id: 19,
+      name: "Zoe",
+      gender: Gender.GIRL,
+      popularity: Popularity.TRENDY,
+      length: Length.SHORT,
+    },
+    {
+      id: 20,
+      name: "Valentina",
+      gender: Gender.GIRL,
+      popularity: Popularity.TRENDY,
+      length: Length.LONG,
+    },
+    {
+      id: 21,
+      name: "Josephine",
+      gender: Gender.GIRL,
+      popularity: Popularity.TRENDY,
+      length: Length.LONG,
+    },
+    {
+      id: 22,
+      name: "Maya",
+      gender: Gender.GIRL,
+      popularity: Popularity.TRENDY,
+      length: Length.SHORT,
+    },
+    {
+      id: 23,
+      name: "Everleigh",
+      gender: Gender.GIRL,
+      popularity: Popularity.UNIQUE,
+      length: Length.LONG,
+    },
+    {
+      id: 24,
+      name: "Poppy",
+      gender: Gender.GIRL,
+      popularity: Popularity.UNIQUE,
+      length: Length.SHORT,
+    },
+    {
+      id: 25,
+      name: "Maia",
+      gender: Gender.GIRL,
+      popularity: Popularity.UNIQUE,
+      length: Length.SHORT,
+    },
+    {
+      id: 26,
+      name: "Ivy",
+      gender: Gender.GIRL,
+      popularity: Popularity.TRENDY,
+      length: Length.SHORT,
+    },
+  
+    {
+      id: 27,
+      name: "Jude",
+      gender: Gender.GIRL,
+      popularity: Popularity.UNIQUE,
+      length: Length.SHORT,
+    },
+    {
+      id: 28,
+      name: "Adrian",
+      gender: Gender.BOY,
+      popularity: Popularity.TRENDY,
+      length: Length.SHORT,
+    },
+    {
+      id: 29,
+      name: "Sunny",
+      gender: Gender.GIRL,
+      popularity: Popularity.UNIQUE,
+      length: Length.SHORT,
+    },
+    {
+      id: 30,
+      name: "Channing",
+      gender: Gender.BOY,
+      popularity: Popularity.TRENDY,
+      length: Length.LONG,
+    },
+    {
+      id: 31,
+      name: "Tennessee",
+      gender: Gender.GIRL,
+      popularity: Popularity.UNIQUE,
+      length: Length.LONG,
+    },
+    {
+      id: 32,
+      name: "Dallas",
+      gender: Gender.BOY,
+      popularity: Popularity.TRENDY,
+      length: Length.SHORT,
+    },
+  
+    {
+      id: 33,
+      name: "Zephyr",
+      gender: Gender.BOY,
+      popularity: Popularity.UNIQUE,
+      length: Length.LONG,
+    },
+  
+    {
+      id: 34,
+      name: "Teri",
+      gender: Gender.GIRL,
+      popularity: Popularity.TRENDY,
+      length: Length.SHORT,
+    },
+  ];
